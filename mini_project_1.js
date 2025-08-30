39a45a5a47f0ace875132ef9e1043e7021a3816e
Vishal Parmar <vparmar30270@gmail.com>
2025-08-30 15:30:25 +0530

    Add files via upload

---
+++ mini_project_1.js
@@ -0,0 +1,8 @@
+const student = {
+    name: "Vishal",
+    age: 21,
+    course: "Computer Science and Information Technology",
+    rollNo: 225,
+    college: "SIRT Bhopal"
+};
+console.log(student)
